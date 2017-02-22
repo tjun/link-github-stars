811f5d7306304e50f49a9d46a632677b988f0746
Junichiro Takagi <t.junichiro@gmail.com>
2017-02-22 21:58:35 +0900

    implement content script

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var reGithub = new RegExp('^https:\/\/github.com\/([^\/]+)/([^\/#]+)$');
+var links = document.getElementsByTagName('a'); // get all links
+$.each($('a'), function() {
+    var url = this.href.toString();
+
+    var res = reGithub.exec(url);
+    if (res === null) { return; }
+    var user = res[1];
+    var repo = res[2];
+    if (user === 'site' || user == 'settings' || user === 'organizations' || user === 'new') {
+        // skip non repo urls
+        return
+    }
+
+    console.log('user:' + user + ', repo:' + repo);
+    var link = this;
+
+    var apiURL = 'https://api.github.com/repos/' + user + '/' + repo;
+    $.getJSON(apiURL, function(data) {
+        link.after($("<span>", {
+            'class': 'link-github-stars',
+            text: '★' + data.stargazers_count,
+        })[0]);
+    });
+})
